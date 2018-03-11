0b1f0ce9e12b4d911c58e8d3a9cff848218fdbed
MeLeRyZ <meleryz@gmail.com>
2018-03-11 16:34:50 +0200

    + make function in controller + add router path + make start template

--- lib/discuss_web/router.ex
+++ lib/discuss_web/router.ex
@@ -19,6 +19,7 @@ defmodule DiscussWeb.Router do
     get "/", TopicController, :index
     get "/topics/new", TopicController, :new #samiy juice
     post "/topics", TopicController, :create
+    get "/topics/:id/edit", TopicController, :edit #wildcardmatcher ":id"
   end
 
   # Other scopes may use custom stacks.
