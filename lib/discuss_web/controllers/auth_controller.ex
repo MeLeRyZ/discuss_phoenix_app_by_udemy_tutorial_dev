710a9533fb8aa3efe0da883b69544cef29a5e64d
John Weaver <meleryz@gmail.com>
2018-07-22 17:43:12 +0200

    auth implementation finished for signin

--- lib/discuss_web/controllers/auth_controller.ex
+++ lib/discuss_web/controllers/auth_controller.ex
@@ -1,12 +1,35 @@
 defmodule DiscussWeb.AuthController do
     use DiscussWeb, :controller
+    alias Discuss.User
     plug Ueberauth
 
-    def callback(conn, params) do
-        IO.puts "++++++++"
-        IO.inspect(conn.assigns)
-        IO.puts "++++++++"
-        IO.inspect(params)
-        IO.puts "++++++++"
+    def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
+        user_params = %{token: auth.credentials.token, email: auth.info.email, provider: "github"}
+        changeset = User.changeset(%User{}, user_params)
+
+        signin(conn, changeset)
+    end
+
+    defp signin(conn, changeset) do
+        case insert_or_update_user(changeset) do
+            {:ok, user} ->
+                conn
+                |> put_flash(:info, "Welcome!")
+                |> put_session(:user_id, user.id) #magic! wohoo!
+                |> redirect(to: topic_path(conn, :index))
+            {:error, _reason} ->
+                conn
+                |> put_flash(:error, "Error signin in")
+                |> redirect(to: topic_path(conn, :index))
+        end
+    end
+
+    defp insert_or_update_user(changeset) do #login user if already exists in db
+        case Repo.get_by(User, email: changeset.changes.email) do
+            nil ->
+                Repo.insert(changeset)
+            user ->
+                {:ok, user}
+        end
     end
 end
